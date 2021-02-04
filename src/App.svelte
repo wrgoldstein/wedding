76894186d50918d777af00e824b67d7cc3800a0e
wrgoldstein <williamrgoldstein@gmail.com>
2021-02-03 19:32:06 -0500

    Add some images

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,53 @@
 <!-- App.svelte -->
 <script>
   import Rsvp from "./Rsvp.svelte"
+  import {flip} from 'svelte/animate';
   let x;
   let click;
   $: url = x > 765 ? "us_sm" : "us_crop"
+
+  const rsvp = async () => click = 'rsvp'
+  const options = {};
 </script>
 <style>
-  /* css will go here */
-  .us {
-    min-width: 367px;
+
+.bg-drawing {
+  z-index: 0;
+}
+
+.bg-drawing:before {
+  background-image: url("us-ftgreen-full.jpg");
+  z-index: 1;
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  content: '';
+  opacity: .55;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+
+@media only screen and (max-width: 600px) {
+  .bg-drawing:before {
+    background-image: url("climbing.jpg")
   }
+}
 </style>
 
 <svelte:window bind:outerWidth={x}/>
-<div class="bg-hero-pattern flex flex-col md:flex-row">
-  <div class="inline-flex md:h-screen ms:w-screen">
+<div class="bg-drawing bg-hero-pattern flex flex-col md:flex-row">
+  <!-- <div class="inline-flex md:h-screen ms:w-screen">
     <img src="/{url}.jpg" alt="us" class="us md:h-full md:w-auto z-50">
-  </div>
-  <div class="inline-flex flex-col md:m-9 bg-drawing p-8 flex-grow rounded">
+  </div> -->
+  <div class="inline-flex flex-col md:m-9 p-8 flex-grow rounded">
     <div class="self-center z-20">
-      <h1 class="text-5xl mt-8 mb-2 text-center antialiased">We're getting married!</h1>
-      <div class="p-8">
+      <h1 class="text-7xl mt-8 mb-2 text-center antialiased">We're getting married!</h1>
+      <div class="p-8 text-xl">
         <ul>
           <li><strong>When</strong>: October 16th, 2021</li>
           <li><strong>Where</strong>: Whiteface Club and Resort, Lake Placid NY</li>
@@ -28,7 +55,7 @@
       </div>
       <div class="flex flex-col items-center self-center">
       {#if click == undefined}
-        <a class='flex px-8 py-3 rounded text-center bg-gray-800 text-white text-sm font-bold' href="#">
+        <a on:click={rsvp} class='flex px-8 py-3 rounded text-center bg-gray-800 text-white text-sm font-bold' href="#">
           RSVP
         </a>
       {:else if click == 'rsvp'}
---
+++ src/Rsvp.svelte
@@ -0,0 +1,19 @@
+<script>
+  let guests;
+  let names;
+
+  const submit = async () => {
+    let res = await fetch("/api/guests", { method: "POST" })
+
+  }
+
+</script>
+
+
+<label for="who">Who are you RSVPing for?</label>
+<input bind:value={names} placeholder="Billy, Bobby, and Buddy (Boondoggles)"/>
+
+{#if names}
+<label for="what">And are you coming?</label>
+<button on:click={submit}></button>
+{/if}
